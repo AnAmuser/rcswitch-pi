cd2c1e16a716b2f3307355ac7b3267b3b816f7ba
Kim Lindhardt Madsen <kim_lindhardt_madsen@hotmail.com>
2014-04-12 16:06:24 +0200

    Update send.cpp

--- send.cpp
+++ send.cpp
@@ -1,6 +1,6 @@
 /*
- Usage: ./send <systemCode> <unitCode> <command>
- Command is 0 for OFF and 1 for ON
+ Usage: ./send <command> <arguments>
+ Command is 0 for BYTE, 1 for DECIMAL, 2 for STRING 
  */
 
 #include "RCSwitch.h"
@@ -10,30 +10,21 @@
 int main(int argc, char *argv[]) {
     
     /*
-     output PIN is hardcoded for testing purposes
-     see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-     for pin mapping of the raspberry pi GPIO connector
+     * output PIN is hardcoded for testing purposes
+     * see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+     * for pin mapping of the raspberry pi GPIO connector
      */
     int PIN = 0;
-    char* systemCode = argv[1];
-    int unitCode = atoi(argv[2]);
-    int command  = atoi(argv[3]);
+    int command = atoi(argv[1]);
+    char* arguments  = atoi(argv[2]);
     
-    if (wiringPiSetup () == -1) return 1;
-	printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
+    if (wiringPiSetup () == -1) {return 1};
+    	
+    	printf("sending command[%i] arguments[%s]\n", command, arguments);
 	RCSwitch mySwitch = RCSwitch();
 	mySwitch.enableTransmit(PIN);
     
-    switch(command) {
-        case 1:
-            mySwitch.switchOn(systemCode, unitCode);
-            break;
-        case 0:
-            mySwitch.switchOff(systemCode, unitCode);
-            break;
-        default:
-            printf("command[%i] is unsupported\n", command);
-            return -1;
-    }
+	mySwitch.send(arguments);
+	
 	return 0;
 }
